6bd28b56b5b8dfb5d37c99eb364c201b5ce2abfa
MYZ <23050151035@aybu.edu.tr>
2026-05-23 03:10:43 +0300

    chore: remove video folder from workspace and git history

--- video/src/ui/SafeArea.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const SafeArea: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <div style={{ position: 'absolute', inset: 0 }}>{children}</div>;
-};
